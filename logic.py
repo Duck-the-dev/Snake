84d42f0a1c1e27c6cf63e7aba94e6376349e1708
Mahmoud moahmed <mahmoud.mohamed30120@gmail.com>
2022-06-25 04:42:50 +0200

    refactor: clean some code and add comments

--- logic.py
+++ logic.py
@@ -20,7 +20,7 @@ class Snake:
             pygame.draw.rect(screen, (183, 110, 125), snake_rect)
 
     def move_snake(self):
-        if self.new_block == True:
+        if self.new_block:
             body_copy = self.body[:]
             body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy
@@ -36,8 +36,8 @@ class Fruit:
         self.randomizer()
 
     def randomizer(self):
-        self.x = random.randint(0, cell_num - 1)
         self.y = random.randint(0, cell_num - 1)
+        self.x = random.randint(0, cell_num - 1)
         self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
@@ -58,36 +58,43 @@ pygame.time.set_timer(ScreenUpdate, 150)
 game_font = pygame.font.Font(None, 35)
 
 lost = False
+
+
 class Main:
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
+
     def again(self):
+        # initiate all objects to their original values
         global lost
         lost = False
         self.snake.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.snake.direction = Vector2(1, 0)
         self.snake.new_block = False
+
     def update(self):
-        if lost == False:
+        if not lost:
             self.snake.move_snake()
             self.check_collision()
             self.check_fail()
 
     def draw_elements(self):
-        if lost == False:
+        if not lost:
             self.fruit.draw_fruit()
             self.snake.draw_snake()
             self.draw_score()
-        if lost == True:
+        if lost:
             self.game_over()
 
     def check_collision(self):
+        # what make you win
         if self.fruit.pos == self.snake.body[0]:
             self.snake.new_block = True
             self.fruit.randomizer()
 
     def check_fail(self):
+        # what makes you lose
         global lost
         if not 0 <= self.snake.body[0].x < cell_num or not 0 <= self.snake.body[0].y < cell_num:
             lost = True
@@ -96,26 +103,29 @@ class Main:
                 lost = True
 
     def game_over(self):
+        # Game Over screen
+
         text = "GameOver"
-        back = pygame.Surface((int(cell_size * cell_num ), int(cell_size * cell_num)))
+        back = pygame.Surface((int(cell_size * cell_num), int(cell_size * cell_num)))
         back.fill(pygame.Color("white"))
+        game_over_surface = game_font.render(text, pygame.Color("black"), True)
+        game_over_x = int(cell_size * cell_num / 2)
+        game_over_y = int(cell_size * cell_num / 2)
+        game_over_rect = game_over_surface.get_rect(center=(game_over_x, game_over_y))
 
-        gameover_sur = game_font.render(text, pygame.Color("black"), True)
-        gameover_x = int(cell_size * cell_num / 2)
-        gameover_y = int(cell_size * cell_num / 2)
-        gameover_rect = gameover_sur.get_rect(center=(gameover_x, gameover_y))
-
-        back_rect = back.get_rect(center=(gameover_x, gameover_y))
+        back_rect = back.get_rect(center=(game_over_x, game_over_y))
         screen.blit(back, back_rect)
 
-        newline_sur = game_font.render("hit the \"Space bar\" to play again or \" Esc\" to Quit", pygame.Color("black"), True)
+        newline_sur = game_font.render("hit the \"Space bar\" to play again or \" Esc\" to Quit",
+                                       pygame.Color("black"),
+                                       True)
 
-        newline_x = int(cell_size * cell_num / 2 )
-        newline_y = int(cell_size * cell_num / 2 + 50 )
+        newline_x = int(cell_size * cell_num / 2)
+        newline_y = int(cell_size * cell_num / 2 + 50)
         newline_rect = newline_sur.get_rect(center=(newline_x, newline_y))
         screen.blit(newline_sur, newline_rect)
 
-        screen.blit(gameover_sur, gameover_rect)
+        screen.blit(game_over_surface, game_over_rect)
         score = "Your score is  : " + str(len(self.snake.body) - 3)
         back = pygame.Surface((120, 30))
         back.fill(pygame.Color("white"))
@@ -124,11 +134,12 @@ class Main:
         score_x = int(cell_size * cell_num / 2)
         score_y = int(cell_size * cell_num / 2 - 60)
 
-
         score_rect = score_sur.get_rect(center=(score_x, score_y))
 
         screen.blit(score_sur, score_rect)
+
     def draw_score(self):
+        # Score box with white background
         score = "Score : " + str(len(self.snake.body) - 3)
         back = pygame.Surface((120, 30))
         back.fill(pygame.Color("white"))
@@ -140,7 +151,6 @@ class Main:
         screen.blit(back, back_rect)
 
         score_rect = score_sur.get_rect(center=(score_x, score_y))
-
         screen.blit(score_sur, score_rect)
 
 
@@ -176,7 +186,6 @@ while True:
                     main_game.snake.direction = Vector2(1, 0)
     screen.fill((174, 215, 69))
     main_game.draw_elements()
-
 
     pygame.display.update()
     clock.tick(60)
